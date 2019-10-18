8d6d1b27d9d68cea8942b9d1064ac87fd76571ce
christopher <1670142089@qq.com>
2019-10-18 21:46:32 +0800

    首页编写

--- miniprogram-yinghuaqi-front/pages/index/index.js
+++ miniprogram-yinghuaqi-front/pages/index/index.js
@@ -7,7 +7,8 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
   },
   //事件处理函数
   bindViewTap: function() {
