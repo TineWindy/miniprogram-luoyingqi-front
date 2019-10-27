377ea68297c03e235b4e928c15b4d7264288af4d
DoneEI <905123393@qq.com>
2019-10-27 21:20:53 +0800

    格式化代码

--- miniprogram-yinghuaqi-front/pages/matchinfo/matchinfo.js
+++ miniprogram-yinghuaqi-front/pages/matchinfo/matchinfo.js
@@ -11,76 +11,76 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
 
 //获取匹配结果
-function getMatchInfo(body){
+function getMatchInfo(body) {
   httpFuncs.yhjRequest(
     '/user/getApplyInfo',
     '',
-    function (res) {
+    function(res) {
       // 获取状态 
-      var status=res.status;
+      var status = res.status;
 
-      if (status =="MATCH_NO_VERIFY"){
+      if (status == "MATCH_NO_VERIFY") {
         // TODO 提示用户去没有报名信息，需要验证
-      } else if (status =="MATCH_NO_SUCCESS"){
+      } else if (status == "MATCH_NO_SUCCESS") {
         // TODO 提示用户匹配未成功
-      } else if (status =="MATCH_CANCELED"){
+      } else if (status == "MATCH_CANCELED") {
         // TODO 提示用户匹配未成功
-      } else if (status =="MATCH_SUCCESS"){
+      } else if (status == "MATCH_SUCCESS") {
         // 渲染
         dataInit(res.lover);
       }
@@ -91,13 +91,13 @@ function getMatchInfo(body){
 }
 
 // 渲染
-function dataInit(data){
+function dataInit(data) {
 
 }
 
 
 // 取消匹配
-function cancelMatchInfo(){
+function cancelMatchInfo() {
   wx.showModal({
     title: '提示',
     content: '您正在取消您的匹配，请谨慎操作',
@@ -106,7 +106,7 @@ function cancelMatchInfo(){
         httpFuncs.yhjRequest(
           '/user/cancelMatch',
           '',
-          function (res) {
+          function(res) {
             wx.showModal({
               title: '取消成功',
               content: '您已取消此次匹配',
@@ -126,11 +126,11 @@ function cancelMatchInfo(){
 }
 
 // 同意匹配
-function agree(){
+function agree() {
   httpFuncs.yhjRequest(
     '/user/agreeMatch',
     '',
-    function (res) {
+    function(res) {
       wx.showModal({
         title: '操作成功',
         content: '您已同意此次匹配',
