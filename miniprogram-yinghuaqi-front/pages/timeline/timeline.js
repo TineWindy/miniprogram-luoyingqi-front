28ea55cdfc9c0ce09c2495a91b108f10811cdeb7
two-wang <www.577107479@qq.com>
2020-01-31 19:49:02 +0800

    新增推荐内容

--- miniprogram-yinghuaqi-front/pages/timeline/timeline.js
+++ miniprogram-yinghuaqi-front/pages/timeline/timeline.js
@@ -7,35 +7,27 @@ var dayTime = require("../../utils/util.js")
 Page({
   //页面的初始数据
   data: {
-    timeDay: '',
-    timeSecond: '',
-    timeWu: '',
+    tab:0,
     list: [{
+      type:"新闻",
+      source:"武汉大学学生会",
+      title:"武汉大学入选2030年世界大学前十",
+      time: "2020/01/28 11:24:53",
+      text:"在2030年CN News等综合性大学排名中，武汉大学获得世界第7名，在中国排名第一",
+      url:""
+    },{
+      type: "新闻",
       source: '大创',
       title: "互联网+大赛通知",
       time: "2020/01/27 17:54:53",
-      timeDay: "",
-      timeSecond: "",
-      timeWu: "",
       text: "下面是互联网+大赛武汉大学校赛报名渠道，点击下方链接进行报名",
       url: ""
     }, {
+      type: "新闻",
       source: '电气青年',
       title: "全国大学生数学建模大赛",
       time: "2020/01/27 16:54:53",
-      timeDay: "",
-      timeSecond: "",
-      timeWu: "",
       text: "下面是大学生数学建模大赛校赛报名渠道，点击下方链接进行报名",
-      url: ""
-    }, {
-      source: 'colorUI',
-      title: "举个栗子",
-      time: "2020/01/24 08:54:53",
-      timeDay: "",
-      timeSecond: "",
-      timeWu: "",
-      text: "这是第一次，我家的铲屎官走了这么久。久到足足有三天！！ 在听到他的脚步声响在楼梯间的那一刻，我简直想要破门而出，对着他狠狠地吼上10分钟，然后再看心情要不要他进门。",
       url: ""
     }],
     list1: []
@@ -44,49 +36,25 @@ Page({
   //生命周期函数--监听页面加载
   onLoad: function(options) {
     //调用接口获取数据
+   
 
 
-
-    //数据处理
-    var list1 = this.data.list.map(function(e) {
-      var timeWu = "";
-      var timeDay = "";
-      var timeSecond = "";
-      //获取时间
-      //var time1 = dayTime.formatTime(e.time);
-      timeDay = e.time.substring(0, 10);
-      timeSecond = e.time.substring(10, 20);
-      ////根据小时数判断上午/中午/下午+
-      var h = e.time.substring(11, 13);
-      //e.time.getHours();
-      if (0 <= h && h <= 6) {
-        timeWu = '凌晨'
-      } else if (6 < h && h <= 11) {
-        timeWu = '上午'
-      } else if (11 < h && h <= 15) {
-        timeWu = '中午'
-      } else if (15 < h && h <= 18) {
-        timeWu = '下午'
-      } else {
-        timeWu = '晚上'
-      };
-      return {
-        source: e.source,
-        title: e.title,
-        time: e.time,
-        timeDay: timeDay,
-        timeSecond: timeSecond,
-        timeWu: timeWu,
-        text: e.text,
-        url: e.url
-      }
-    })
-    this.setData({
-      list1: list1
-    })
   },
-
+  //进入详情页面
   toInfo: function() {
 
+  },
+  //改变tab页为“推荐”
+  changeTabTo_0:function(){
+    this.setData({
+      tab:0
+    })
+  },
+  //改变tab页为“活动”
+  changeTabTo_1: function () {
+    this.setData({
+      tab: 1
+    })
   }
+
 })
