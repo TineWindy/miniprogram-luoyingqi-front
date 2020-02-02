b23c0424d005d2d55619041584ce7c01e6079f56
two-wang <www.577107479@qq.com>
2020-02-02 21:53:24 +0800

    完善推荐页面，获取服务器数据

--- miniprogram-yinghuaqi-front/pages/timeline/timeline.js
+++ miniprogram-yinghuaqi-front/pages/timeline/timeline.js
@@ -7,45 +7,25 @@ var dayTime = require("../../utils/util.js")
 Page({
   //页面的初始数据
   data: {
+    page:0,
     tab:0,
-    list: [{
-      type:"新闻",
-      source:"武汉大学学生会",
-      title:"武汉大学入选2030年世界大学前十",
-      time: "2020/01/28 11:24:53",
-      text:"在2030年CN News等综合性大学排名中，武汉大学获得世界第7名，在中国排名第一",
-      url:""
-    },{
-      type: "新闻",
-      source: '大创',
-      title: "互联网+大赛通知",
-      time: "2020/01/27 17:54:53",
-      text: "下面是互联网+大赛武汉大学校赛报名渠道，点击下方链接进行报名",
-      url: ""
-    }, {
-      type: "新闻",
-      source: '电气青年',
-      title: "全国大学生数学建模大赛",
-      time: "2020/01/27 16:54:53",
-      text: "下面是大学生数学建模大赛校赛报名渠道，点击下方链接进行报名",
-      url: ""
-    }],
+    list: [],
     list1: []
   },
-
   //生命周期函数--监听页面加载
   onLoad: function(options) {
-    //调用接口获取数据
-   
-
-
+    getTotalList(this);
+  },
+  //触底加载
+  onReachBottom:function(){
+    getMoreList(this);
   },
   //进入详情页面
-  toInfo: function() {
-
+  toDetail: function(e) {
+    myToDetail(this,e)
   },
   //改变tab页为“推荐”
-  changeTabTo_0:function(){
+  changeTabTo_0:function(e){
     this.setData({
       tab:0
     })
@@ -56,5 +36,72 @@ Page({
       tab: 1
     })
   }
-
 })
+//获取数据列表
+function getTotalList(body){
+  let nextPage = body.data.page+1;
+  httpFuncs.yhjRequest(
+    '/timeline/getAllTimeline',{page:body.data.page},
+    function (res){
+      let _list = res.resultObj.map(e=>myPublishTime(e))
+      body.setData({
+        list:_list,
+        page:nextPage
+      })
+    },
+    'GET'
+  );
+}
+//触底增加list数据
+function getMoreList(body){
+  let oldList = body.data.list;
+  if(oldList.length<=120){
+    let nextPage = body.data.page + 1;
+    httpFuncs.yhjRequest(
+      '/timeline/getAllTimeline', { page: body.data.page},
+      function (res) {
+        let _list = res.resultObj.map(e=>myPublishTime(e))
+        body.setData({
+          list: oldList.concat(_list),
+          page: nextPage
+        })
+      },
+      'GET'
+    );
+  } else{
+    wx.showToast({
+      title: '我是有底线滴',
+      icon:'none'
+    })
+  }
+}
+//进入详情页面
+function myToDetail(body,e){
+  var id = e.currentTarget.id
+  wx.navigateTo({
+    url: '../detail/detail?id' + id,
+  }) 
+}
+//publishTime的处理
+function myPublishTime(e){
+  let date = new Date(e.publishTime);
+  let nowDate = new Date();
+  let _publishTime = dayTime.formatTime(date);
+  let _nowDate = dayTime.formatTime(nowDate);
+  if (_publishTime.substring(0, 10) == _nowDate.substring(0, 10)) {
+    _publishTime = '今天  ' + _publishTime.substring(11, 16)
+  } else if (_publishTime.substring(0, 7) == _nowDate.substring(0, 7) && Number(_nowDate.substring(8, 10)) - Number(_publishTime.substring(8, 10)) == 1) {
+    _publishTime = '昨天  ' + _publishTime.substring(11, 16)
+  } else {
+    _publishTime = _publishTime.substring(0, 16)
+  }
+  return {
+    id: e.id,
+    digest: e.digest,
+    contentUrl: e.contentUrl,
+    publishTime: _publishTime,
+    source: e.source,
+    title: e.title,
+    type: e.type
+  }
+}
