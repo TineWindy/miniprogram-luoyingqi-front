f017c110b45303749699ed8e59824e9e4df8ea14
two-wang <www.577107479@qq.com>
2020-11-11 12:54:22 +0800

    优化定向越野页面

--- miniprogram-yinghuaqi-front/pages/task/orienteering/orienteering.js
+++ miniprogram-yinghuaqi-front/pages/task/orienteering/orienteering.js
@@ -4,7 +4,7 @@ Page({
   data: {
     personalTask:[{
       taskId:0,
-      taskStatus:'done',
+      taskStatus:'FINISHED',
       taskLocal:"武汉大学牌坊",
       taskPeople:"王二威",
       taskTime:"9:12",
@@ -12,16 +12,33 @@ Page({
       taskScore:60
     },{
       taskId:0,
-      taskStatus:'notDone',
+      taskStatus:'FINISHED',
       taskLocal:"武汉大学牌坊",
       taskPeople:"王二威",
+      taskTime:"9:12",
+      taskReward:"一张卡片",
+      taskScore:60
+    }],
+    otherTask:[{
+      taskId:0,
+      taskStatus:'FINISHED',
+      taskLocal:"武汉大学奥场",
+      taskPeople:"邓六水",
+      taskTime:"9:12",
+      taskReward:"一张卡片",
+      taskScore:60
+    },{
+      taskId:0,
+      taskStatus:'STARTED',
+      taskLocal:"武汉大学奥场",
+      taskPeople:"邓六水",
       taskTime:"9:12",
       taskReward:"一张卡片",
       taskScore:60
     }],
     teamTask:[{
       taskId:0,
-      taskStatus:'done',
+      taskStatus:'CANCELED',
       taskLocal:"武汉大学牌坊",
       taskPeople:"王二威",
       taskTime:"9:12",
@@ -29,18 +46,26 @@ Page({
       taskScore:60
     },{
       taskId:0,
-      taskStatus:'notDone',
+      taskStatus:'CANCELED',
       taskLocal:"武汉大学牌坊",
       taskPeople:"王二威",
       taskTime:"9:12",
       taskReward:"一张卡片",
       taskScore:60
     }],
+    status:"NOT_RUNNING",
+    tabIndex:0,
     tabBarIndex:0,
     modalShow:false
   },
   onLoad: function (options) {
 
+  },
+  //切换单人任务的目标任务
+  changeTab:function(e){
+    this.setData({
+      tabIndex:e.currentTarget.dataset.index
+    })
   },
   //切换任务
   changeTabCur:function(e){
@@ -71,6 +96,27 @@ function changeTabCurIndex(body,e){
       tabBarIndex:0
     })
   }else{
+    if(e.currentTarget.dataset.index < 2 && body.data.tabIndex == 0 && body.data.personalTask[e.currentTarget.dataset.index].taskStatus == 'CANCELED'){
+      wx.showToast({
+        title: '暂未开始此任务~',
+        icon:"none"
+      })
+      return 
+    }
+    if(e.currentTarget.dataset.index < 2 && body.data.tabIndex ==  1 && body.data.otherTask[e.currentTarget.dataset.index].taskStatus == 'CANCELED'){
+      wx.showToast({
+        title: '暂未开始此任务~',
+        icon:"none"
+      })
+      return 
+    }
+    if(e.currentTarget.dataset.index > 1 && body.data.teamTask[e.currentTarget.dataset.index -2 ].taskStatus == 'CANCELED'){
+      wx.showToast({
+        title: '暂未开始此任务~',
+        icon:"none"
+      })
+      return 
+    }
     body.setData({
       tabBarIndex:e.currentTarget.dataset.index+1
     })
